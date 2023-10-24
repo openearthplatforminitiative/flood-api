30343dd4b50bfd34acb0d1462b394d2fc7d86da0
Erik Dymbe <erik.dymbe@knowit.no>
2023-10-24 13:50:05 +0200

    feat: add database deployment

--- flood_api/routers/healthcheck.py
+++ flood_api/routers/healthcheck.py
@@ -1,8 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi import Response
+from healthcheck import HealthCheck
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from flood_api.db import database
+from flood_api.settings import settings
 
 router = APIRouter(tags=["health"])
 
 
 @router.get("/health")
-async def liveness() -> dict[str, str]:
+def liveness() -> dict[str, str]:
     return {"message": "Ok"}
+
+
+@router.get("/ready", tags=["health"])
+async def ready(session: Session = Depends(database.get_session)) -> Response:
+    def database_available() -> (bool, str):
+        try:
+            version = session.execute(text("SELECT version()")).scalar()
+            return True, version
+        except Exception as e:
+            print(e)
+            return False, "Not Available"
+
+    health = HealthCheck()
+
+    health.add_section("version", settings.version)
+    health.add_check(database_available)
+
+    message, status_code, headers = health.run()
+    return Response(content=message, headers=headers, status_code=status_code)
--- flood_api/settings.py
+++ flood_api/settings.py
@@ -7,3 +7,9 @@ class Settings(BaseSettings):
     uvicorn_host: str = "0.0.0.0"
     uvicorn_reload: bool = True
     uvicorn_proxy_headers: bool = False
+    database_url: str = (
+        "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+    )
+
+
+settings = Settings()
