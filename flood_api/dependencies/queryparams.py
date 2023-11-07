68d45ccba10e4bae0fe865dd73bcb44dfcac5245
Erik Dymbe <erik.dymbe@knowit.no>
2023-11-07 15:23:51 +0100

    fix: document query-parameters

--- flood_api/dependencies/queryparams.py
+++ flood_api/dependencies/queryparams.py
@@ -1,19 +1,40 @@
 from typing import Annotated
 from datetime import date
 
-from fastapi import Depends
+from fastapi import Depends, Query
 
 
-def coordinates(lat: float, lon: float) -> (float, float):
-    return lat, lon
+def coordinates(
+    lon: Annotated[
+        float,
+        Query(description="Longitude of the coordinate to retrieve data for"),
+    ],
+    lat: Annotated[
+        float,
+        Query(description="Latitude of the coordinate to retrieve data for"),
+    ],
+) -> (float, float):
+    return lon, lat
 
 
 CoordinatesDep = Annotated[tuple[float, float], Depends(coordinates)]
 
 
 def date_range(
-    start_date: date | None = None,
-    end_date: date | None = None,
+    start_date: Annotated[
+        date | None,
+        Query(
+            description="Inclusive lower bound for the range of dates to return data for. If omitted the date range "
+            "will not have a lower bound"
+        ),
+    ] = None,
+    end_date: Annotated[
+        date | None,
+        Query(
+            description="Inclusive upper bound for the range of dates to return data for. If omitted the date range "
+            "will not have an upper bound"
+        ),
+    ] = None,
 ) -> (date, date):
     if start_date is None:
         start_date = date.min
--- flood_api/routers/flood.py
+++ flood_api/routers/flood.py
@@ -3,18 +3,18 @@ from fastapi import APIRouter
 from flood_api.dependencies.flooddata import DetailedFloodDataDep
 from flood_api.dependencies.queryparams import CoordinatesDep, DateRangeDep
 
-router = APIRouter(prefix="/flood", tags=["flood"])
+router = APIRouter(tags=["flood"])
 
 
 @router.get("/detailed")
 async def detailed(
     data: DetailedFloodDataDep, coordinates: CoordinatesDep, date_range: DateRangeDep
 ):
-    lat, lon = coordinates
+    lon, lat = coordinates
     start_date, end_date = date_range
     return {
-        "lat": lat,
         "lon": lon,
+        "lat": lat,
         "start_date": start_date,
         "end_date": end_date,
         "count": len(data),
