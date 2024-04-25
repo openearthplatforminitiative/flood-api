1d34a432c5cbd606e9ef05cd8074d265a0e19868
Erik Dymbe <erik.dymbe@knowit.no>
2024-04-25 12:18:46 +0200

    feat: inject dagster data bucket via configmap

--- flood_api/settings.py
+++ flood_api/settings.py
@@ -7,8 +7,9 @@ class Settings(BaseSettings):
     uvicorn_host: str = "0.0.0.0"
     uvicorn_reload: bool = True
     uvicorn_proxy_headers: bool = False
-    detailed_data_path: str = "s3://databricks-data-openepi/glofas/processed/newest/processed_detailed_forecast.parquet/"
-    summary_data_path: str = "s3://databricks-data-openepi/glofas/processed/newest/processed_summary_forecast.parquet/"
+    dagster_data_bucket: str = "databricks-data-openepi"
+    detailed_data_path: str = f"s3://{dagster_data_bucket}/glofas/processed/newest/processed_detailed_forecast.parquet/"
+    summary_data_path: str = f"s3://{dagster_data_bucket}/glofas/processed/newest/processed_summary_forecast.parquet/"
     threshold_data_path: str = "s3://databricks-data-openepi/glofas/auxiliary-data/processed_thresholds.parquet/"
     api_root_path: str = ""
     api_description: str = (
