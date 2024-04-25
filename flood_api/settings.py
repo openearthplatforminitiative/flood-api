6565af1c45325e03ed4dd0f680148799227c99e4
Gil Tinde <31830765+giltinde@users.noreply.github.com>
2024-04-25 13:31:49 +0200

    fix: add default bucket name so tests succeed

--- flood_api/settings.py
+++ flood_api/settings.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     uvicorn_host: str = "0.0.0.0"
     uvicorn_reload: bool = True
     uvicorn_proxy_headers: bool = False
-    dagster_data_bucket: str = environ.get("dagster_data_bucket")
+    dagster_data_bucket: str = environ.get("dagster_data_bucket", "placeholder-bucket")
     detailed_data_path: str = f"s3://{dagster_data_bucket}/detailed_forecast.parquet/"
     summary_data_path: str = f"s3://{dagster_data_bucket}/summary_forecast.parquet/"
     threshold_data_path: str = (
